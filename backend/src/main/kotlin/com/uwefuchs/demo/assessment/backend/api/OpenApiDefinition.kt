ab57e7e8153e59c24cbaf2be6733eaaedc2028ce
Uwe Fuchs <info@uwefuchs.com>
2025-10-29 11:35:06 +0100

    add OpenApiDefinition

---
+++ backend/src/main/kotlin/com/uwefuchs/demo/assessment/backend/api/OpenApiDefinition.kt
@@ -0,0 +1,9 @@
+package com.uwefuchs.demo.assessment.backend.api
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+
+@OpenAPIDefinition(info = Info(title = "Assecor Assessment API", version = "v1"))
+class OpenApiDefinition
